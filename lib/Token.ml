4f4f507687aba29e0acb2a50df20467768cf3e0f
Torben Ewert <torben@ewert-online.com>
2021-12-25 18:26:52 +0100

    print tokens more readable

--- lib/Token.ml
+++ lib/Token.ml
@@ -55,52 +55,52 @@ type t = { typ : token_type; start_pos : Position.t; end_pos : Position.t }
 let make ~start_pos ~end_pos typ = { typ; start_pos; end_pos }
 
 let to_string = function
-  | FLOAT f -> Printf.sprintf "Float: %f" f
-  | INT i -> Printf.sprintf "Int: %i" i
-  | STRING s -> Printf.sprintf "String: \"%s\"" s
-  | KEYWORD_TRUE -> "true"
-  | KEYWORD_FALSE -> "false"
-  | LEFT_PAREN -> "("
-  | RIGHT_PAREN -> ")"
-  | LEFT_BRACE -> "{"
-  | RIGHT_BRACE -> "}"
-  | LEFT_BRACK -> "["
-  | RIGHT_BRACK -> "]"
-  | SEMICOLON -> ";"
-  | COLON -> ":"
-  | COMMA -> ","
-  | DOT -> "."
-  | PIPE -> "|"
-  | EQUAL -> "="
-  | NOT_EQUAL -> "!="
-  | EQUAL_EQUAL -> "=="
-  | LOGICAL_AND -> "&&"
-  | LOGICAL_OR -> "||"
-  | NOT -> "!"
-  | GREATER -> ">"
-  | GREATER_EQUAL -> ">="
-  | LESS -> "<"
-  | LESS_EQUAL -> "<="
-  | PLUS -> "+"
-  | MINUS -> "-"
-  | STAR -> "*"
-  | STAR_STAR -> "**"
-  | SLASH -> "/"
-  | PERCENT -> "%"
-  | KEYWORD_IF -> "if"
-  | KEYWORD_ELSE -> "else"
-  | KEYWORD_FOR -> "for"
-  | KEYWORD_IN -> "in"
-  | KEYWORD_BREAK -> "break"
-  | KEYWORD_CONTINUE -> "continue"
-  | KEYWORD_COMPONENT -> "component"
-  | KEYWORD_SITE -> "site"
-  | KEYWORD_PAGE -> "page"
-  | KEYWORD_STORE -> "store"
-  | SYMBOL s -> Printf.sprintf "Symbol: %s" s
-  | IDENT_LOWER s -> Printf.sprintf "Lower Ident: %s" s
-  | IDENT_UPPER s -> Printf.sprintf "Upper Ident: %s" s
-  | END_OF_INPUT -> "(THE_END)"
+  | FLOAT f           -> Printf.sprintf "Token.FLOAT: ........... %f" f
+  | INT i             -> Printf.sprintf "Token.INT: ............. %i" i
+  | STRING s          -> Printf.sprintf "Token.STRING: .......... %s" s
+  | SYMBOL s          -> Printf.sprintf "Token.SYMBOL: .......... %s" s
+  | IDENT_LOWER s     -> Printf.sprintf "Token.IDENT_LOWER: ..... %s" s
+  | IDENT_UPPER s     -> Printf.sprintf "Token.IDENT_UPPER: ..... %s" s
+  | KEYWORD_TRUE      -> "Token.KEYWORD_TRUE"
+  | KEYWORD_FALSE     -> "Token.KEYWORD_FALSE"
+  | LEFT_PAREN        -> "Token.LEFT_PAREN"
+  | RIGHT_PAREN       -> "Token.RIGHT_PAREN"
+  | LEFT_BRACE        -> "Token.LEFT_BRACE"
+  | RIGHT_BRACE       -> "Token.RIGHT_BRACE"
+  | LEFT_BRACK        -> "Token.LEFT_BRACK"
+  | RIGHT_BRACK       -> "Token.RIGHT_BRACK"
+  | SEMICOLON         -> "Token.SEMICOLON"
+  | COLON             -> "Token.COLON"
+  | COMMA             -> "Token.COMMA"
+  | DOT               -> "Token.DOT"
+  | PIPE              -> "Token.PIPE"
+  | EQUAL             -> "Token.EQUAL"
+  | NOT_EQUAL         -> "Token.NOT_EQUAL"
+  | EQUAL_EQUAL       -> "Token.EQUAL_EQUAL"
+  | LOGICAL_AND       -> "Token.LOGICAL_AND"
+  | LOGICAL_OR        -> "Token.LOGICAL_OR"
+  | NOT               -> "Token.NOT"
+  | GREATER           -> "Token.GREATER"
+  | GREATER_EQUAL     -> "Token.GREATER_EQUAL"
+  | LESS              -> "Token.LESS"
+  | LESS_EQUAL        -> "Token.LESS_EQUAL"
+  | PLUS              -> "Token.PLUS"
+  | MINUS             -> "Token.MINUS"
+  | STAR              -> "Token.STAR"
+  | STAR_STAR         -> "Token.STAR_STAR"
+  | SLASH             -> "Token.SLASH"
+  | PERCENT           -> "Token.PERCENT"
+  | KEYWORD_IF        -> "Token.KEYWORD_IF"
+  | KEYWORD_ELSE      -> "Token.KEYWORD_ELSE"
+  | KEYWORD_FOR       -> "Token.KEYWORD_FOR"
+  | KEYWORD_IN        -> "Token.KEYWORD_IN"
+  | KEYWORD_BREAK     -> "Token.KEYWORD_BREAK"
+  | KEYWORD_CONTINUE  -> "Token.KEYWORD_CONTINUE"
+  | KEYWORD_COMPONENT -> "Token.KEYWORD_COMPONENT"
+  | KEYWORD_SITE      -> "Token.KEYWORD_SITE"
+  | KEYWORD_PAGE      -> "Token.KEYWORD_PAGE"
+  | KEYWORD_STORE     -> "Token.KEYWORD_STORE"
+  | END_OF_INPUT      -> "Token.END_OF_INPUT"
 
 let keyword_of_string = function
 | "true" -> Some KEYWORD_TRUE
--- test/test.t/run.t
+++ test/test.t/run.t
@@ -1,50 +1,51 @@
   $ esy x print_tokens ./data.fe
-  Symbol: Ui
-  (
-  Lower Ident: label
-  :
-  String: "Docs Site"
-  ,
-  Lower Ident: icon
-  :
-  String: "/images/icons/site-docs.svg"
-  )
-  Symbol: Domain
-  (
-  Lower Ident: main
-  :
-  String: "docs.fennek-cms.com"
-  ,
-  Lower Ident: additional
-  :
-  [
-  String: "docs.fennek-cms.de"
-  ,
-  String: "docs.fennek-cms.fr"
-  ]
-  )
-  site
-  Upper Ident: Docs
-  {
-  Lower Ident: floats
-  =
-  [
-  Float: 1000.462000
-  ,
-  Float: 1.000000
-  ,
-  Float: 1.433200
-  ,
-  Float: 0.500000
-  ,
-  Float: 10000000000.000000
-  ,
-  Float: 100.000000
-  ,
-  Float: 0.000010
-  ,
-  Float: 0.010000
-  ,
-  Float: 0.010000
-  ]
-  }
+  Token.SYMBOL: .......... Ui
+  Token.LEFT_PAREN
+  Token.IDENT_LOWER: ..... label
+  Token.COLON
+  Token.STRING: .......... Docs Site
+  Token.COMMA
+  Token.IDENT_LOWER: ..... icon
+  Token.COLON
+  Token.STRING: .......... /images/icons/site-docs.svg
+  Token.RIGHT_PAREN
+  Token.SYMBOL: .......... Domain
+  Token.LEFT_PAREN
+  Token.IDENT_LOWER: ..... main
+  Token.COLON
+  Token.STRING: .......... docs.fennek-cms.com
+  Token.COMMA
+  Token.IDENT_LOWER: ..... additional
+  Token.COLON
+  Token.LEFT_BRACK
+  Token.STRING: .......... docs.fennek-cms.de
+  Token.COMMA
+  Token.STRING: .......... docs.fennek-cms.fr
+  Token.RIGHT_BRACK
+  Token.RIGHT_PAREN
+  Token.KEYWORD_SITE
+  Token.IDENT_UPPER: ..... Docs
+  Token.LEFT_BRACE
+  Token.IDENT_LOWER: ..... floats
+  Token.EQUAL
+  Token.LEFT_BRACK
+  Token.FLOAT: ........... 1000.462000
+  Token.COMMA
+  Token.FLOAT: ........... 1.000000
+  Token.COMMA
+  Token.FLOAT: ........... 1.433200
+  Token.COMMA
+  Token.FLOAT: ........... 0.500000
+  Token.COMMA
+  Token.FLOAT: ........... 10000000000.000000
+  Token.COMMA
+  Token.FLOAT: ........... 100.000000
+  Token.COMMA
+  Token.FLOAT: ........... 0.000010
+  Token.COMMA
+  Token.FLOAT: ........... 0.010000
+  Token.COMMA
+  Token.FLOAT: ........... 0.010000
+  Token.RIGHT_BRACK
+  Token.SEMICOLON
+  Token.RIGHT_BRACE
