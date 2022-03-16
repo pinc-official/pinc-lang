23904f12598a0a996b9fa502e32b3b736e21281a
Torben Ewert <torben@ewert-online.com>
2022-03-16 17:43:04 +0100

    fix debug printer

--- bin/print_debug.ml
+++ bin/print_debug.ml
@@ -2,10 +2,15 @@ open Pinc_lib
 
 let main =
   let filename = Sys.argv.(1) in
+  let root = Sys.argv.(2) in
   let start_time = Unix.gettimeofday () in
   let ast = Parser.parse_file filename in
   let end_time_parser = Unix.gettimeofday () in
-  let result = Interpreter.from_ast ast in
+  let result =
+    Interpreter.from_ast ast root
+    |> Interpreter.Output.get_value
+    |> Interpreter.Value.to_string
+  in
   let end_time = Unix.gettimeofday () in
   Printf.printf "Lexer & Parser: %fms\n" ((end_time_parser -. start_time) *. 1000.);
   Printf.printf "Interpreter:    %fms\n" ((end_time -. end_time_parser) *. 1000.);
