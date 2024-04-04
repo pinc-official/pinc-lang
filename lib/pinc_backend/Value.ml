48bbffa358265bb33f30f015ca50ad07bcc28d27
Torben Ewert <torben@ewert-online.com>
2024-04-04 20:05:23 +0200

    remove duplicate value constructors

--- lib/pinc_backend/Value.ml
+++ lib/pinc_backend/Value.ml
@@ -1,38 +1,6 @@
 module Diagnostics = Pinc_Diagnostics
 include Types.Type_Value
 
-let null ?(value_loc = Diagnostics.Location.none) () = { value_loc; value_desc = Null }
-
-let of_char ?(value_loc = Diagnostics.Location.none) c =
-  { value_loc; value_desc = Char c }
-;;
-
-let of_string ?(value_loc = Diagnostics.Location.none) s =
-  { value_loc; value_desc = String s }
-;;
-
-let of_bool ?(value_loc = Diagnostics.Location.none) b =
-  { value_loc; value_desc = Bool b }
-;;
-
-let of_int ?(value_loc = Diagnostics.Location.none) i = { value_loc; value_desc = Int i }
-
-let of_float ?(value_loc = Diagnostics.Location.none) f =
-  { value_loc; value_desc = Float f }
-;;
-
-let of_array ?(value_loc = Diagnostics.Location.none) l =
-  { value_loc; value_desc = Array l }
-;;
-
-let of_list ?(value_loc = Diagnostics.Location.none) l =
-  { value_loc; value_desc = Array (Array.of_list l) }
-;;
-
-let of_string_map ?(value_loc = Diagnostics.Location.none) m =
-  { value_loc; value_desc = Record m }
-;;
-
 let rec to_string value =
   match value.value_desc with
   | Portal list -> list |> List.rev_map to_string |> String.concat "\n"
